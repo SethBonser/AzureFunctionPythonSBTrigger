98b857a45e17512a1a4155053ae74a4bf3260cff
Seth Bonser <sbonser34@gmail.com>
2025-04-03 11:39:51 -0600

    first commit

---
+++ function_app.py
@@ -0,0 +1,10 @@
+import azure.functions as func
+import logging
+
+app = func.FunctionApp()
+
+@app.service_bus_queue_trigger(arg_name="azservicebus", queue_name="mysbqueue",
+                               connection="SERVICE_BUS_CONNECTION") 
+def servicebus_queue_trigger(azservicebus: func.ServiceBusMessage):
+    logging.info('Python ServiceBus Queue trigger processed a message: %s',
+                azservicebus.get_body().decode('utf-8'))
